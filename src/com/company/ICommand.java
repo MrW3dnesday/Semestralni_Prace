c39fb25d4629d2bc2e22a26d10d023c6e00dc838
Patrik Doubek <patrik@doubek.cz>
2021-05-18 12:49:19 +0200

    Java Doc on ICommand and IItem

--- src/com/company/ICommand.java
+++ src/com/company/ICommand.java
@@ -1,5 +1,14 @@
 package com.company;
 
+/*
+ *  Interface ICommand - TODO
+ *
+ *
+ *
+ *  @author     Dan Šebek
+ *  @version    0.01a
+ *  @created    květen 2021
+ */
 public interface ICommand {
     public void Execute();
 }
--- src/com/company/IItem.java
+++ src/com/company/IItem.java
@@ -1,5 +1,14 @@
 package com.company;
 
+/*
+ *  Interface IItem - TODO
+ *
+ *
+ *
+ *  @author     Dan Šebek
+ *  @version    0.01a
+ *  @created    květen 2021
+ */
 public interface IItem {
     public String OnInspect();
 }
