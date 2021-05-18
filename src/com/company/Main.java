96334138754f25811a2503c135f4b8da741d1fde
MrW3dnesday <31487011+MrW3dnesday@users.noreply.github.com>
2021-05-18 12:38:55 +0100

    fe

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,14 @@ package com.company;
 
 public class Main {
 
+
+    //PatO vydis tuhle mrdku
+
     public static void main(String[] args) {
 	// TODO
     //naser si dane xD]
 
-
+//to vis ze jo ty hovno
 
 
     }
