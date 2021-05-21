628da48b8b2e1ca07fa2ac8d7afe2ef07e763cf0
Patrik Doubek <patrik@doubek.cz>
2021-05-21 16:36:47 +0200

    added shitload of comments

--- src/com/company/Backpack.java
+++ src/com/company/Backpack.java
@@ -30,9 +30,9 @@ public class Backpack {
      * @return Vrací příznak provedení
      */
     public char OnPutIn(IItem item){
-        //check if itam can be put in backpack
+        //check if item can be put in backpack
         if(item.CanBeInBackpack()){
-            //check if backapck is full
+            //check if backpack is full
             if(itemsInBackpack.size() <= maxItems){
                 //check if item is already in backpack
                 if(!itemsInBackpack.containsKey(item.GetItemName())){
@@ -50,6 +50,11 @@ public class Backpack {
 
     }
 
+    /**
+     * Metoda zjistí zda je v batohu předmět s kterým se dá interagovat
+     *
+     * @return Vrací příznak zda v batohu jsou předemty s který jde nějak interagovat
+     */
     public char HaveInteractableItem(){
         int temp = itemsInBackpack.size();
         if(temp == 0){
@@ -59,12 +64,21 @@ public class Backpack {
     }
 
 
-
+    /**
+     * Metoda zajistí vyndání předmětu z batohu
+     *
+     * @return Vrací vyndaný předmět
+     */
     public IItem OnTakeOut(String itemName){
         IItem removedItem = itemsInBackpack.remove(itemName);
         return removedItem;
     }
 
+    /**
+     * Metoda připraví text
+     *
+     * @return Vrací vyndaný předmět
+     */
     public String ShowItemsInBackpack(){
         String temp = "";
         for(String key: this.itemsInBackpack.keySet()){
