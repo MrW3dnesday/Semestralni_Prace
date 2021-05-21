f7948208aa9739d9fb168c6c19b47d36936ca10d
Patrik Doubek <patrik@doubek.cz>
2021-05-21 17:39:07 +0200

    added shitload of comments

--- src/com/company/Mimic.java
+++ src/com/company/Mimic.java
@@ -31,6 +31,10 @@ public class Mimic extends Item implements IItem {
      *
      */
     private void ChangeForm(){
+        //Possible problem
+        //If there is only one room left without mimic and it has all item types inside this will end into never ending loop
+        //Possible solution
+        //Change form to standard mimic form
         IItem[] possibleItems = GamePlan.generatedItems.values().toArray(new IItem[GamePlan.generatedItems.size()]);
         Random random = new Random();
         boolean validForm = false;
@@ -59,9 +63,15 @@ public class Mimic extends Item implements IItem {
     }
 
     public void OnMove(){
-        //get room that mimic can move too
+        //Possible problem
+        //If there is none room left without mimic this will end in never ending loop
+        //Possible Solution
+        //Let there me more mimics in one room. Possibly less more interaction with mimics.
+
+        //get array of rooms that mimic can move too
         BasicRoom[] connectedRooms = currentRoom.GetConnectedRooms().values().toArray(new BasicRoom[currentRoom.GetConnectedRooms().size()]);
         Random random = new Random();
+        //get random room from array
         BasicRoom newRoom = connectedRooms[random.nextInt(connectedRooms.length)];
 
 
