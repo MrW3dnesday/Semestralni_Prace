24e70242f6032ee9a896bfc3d2286b70e30ee785
Patrik Doubek <patrik@doubek.cz>
2021-05-21 18:14:53 +0200

    added shitload of comments

--- src/com/company/Mimic.java
+++ src/com/company/Mimic.java
@@ -27,8 +27,7 @@ public class Mimic extends Item implements IItem {
     }
 
     /**
-     * Metoda zajišťuje přesun Mimic do další místnosti a změnu jeji formy na předmět který v místnosti jěště není
-     *
+     * Metoda zajišťuje změnu formu mimic
      */
     private void ChangeForm(){
         //Possible problem
@@ -39,18 +38,24 @@ public class Mimic extends Item implements IItem {
         Random random = new Random();
         boolean validForm = false;
         IItem temp;
+
         for(int i = 0;i<possibleItems.length;i++){
+            //Get random item from array
             temp = possibleItems[random.nextInt(possibleItems.length)];
+            //check if the item is in room
             if(!currentRoom.GetItemsInRoom().containsKey(temp.GetItemName())){
+                //set mimic form
                 this.itemName = temp.GetItemName();
                 this.itemDescription = temp.GetItemDescription();
                 validForm = true;
                 break;
             }
         }
+        //if there is not possible form form mimic, move to next room
         if(!validForm){
             OnMove();
         }
+        //Obsolete Solution
         /*while(!validForm){
             temp = possibleItems[random.nextInt(possibleItems.length)];
             if(!currentRoom.GetItemsInRoom().containsKey(temp.GetItemName())){
@@ -62,6 +67,9 @@ public class Mimic extends Item implements IItem {
         }*/
     }
 
+    /**
+     * Metoda zajišťuje přesun Mimic do další místnosti a změnu jeji formy na předmět který v místnosti jěště není
+     */
     public void OnMove(){
         //Possible problem
         //If there is none room left without mimic this will end in never ending loop
@@ -79,7 +87,9 @@ public class Mimic extends Item implements IItem {
 
         IItem temp;
 
+        //check if mimic current room has connected rooms
         if(newRoom.GetConnectedRooms().size() > 0) {
+            //check if connected room has mimic inside
             for (String key : newRoom.GetItemsInRoom().keySet()) {
                 temp = newRoom.GetItemsInRoom().get(key).get(0);
                 if (temp.GetItemEngineName().equals("mimic")) {
@@ -87,35 +97,57 @@ public class Mimic extends Item implements IItem {
                 }
             }
 
+            //move mimic to next room
             currentRoom.OnItemRemove(this.itemName);
             currentRoom = newRoom;
             ChangeForm();
             currentRoom.AddItemInRoom(this);
 
+            //if the room has mimic inside, move mimic to next room
             if(hasMimic){
                 OnMove();
             }
         }
     }
 
+    /**
+     * Metoda vrátí popis předmětu @Override
+     *
+     *@return Vrací popis předmětu
+     */
     @Override
     public String OnInspect(){
 
         return "Je to mimic. To bude bolet!" + GamePlan.GetPlayer().OnPlayerAttacked();
     }
 
+    /**
+     * Metoda vrátí text při vložení do batohu, a zaútočí na hráče @Override
+     *
+     *@return Vrací popis předmětu
+     */
     @Override
     public String OnItemPutInBackpack(){
 
         return "Je to mimic. Moc se jí tam nechce. To bude bolet!" +  GamePlan.GetPlayer().OnPlayerAttacked();
     }
 
+    /**
+     * Metoda vrátí text při interakci, a zaútočí na hráče @Override
+     *
+     *@return Vratí text při interakci
+     */
     @Override
     public OnInteractionReturn OnInteract(){
-        OnInteractionReturn temp = new OnInteractionReturn(false,"Je to mimic. To bude bolet!");
+        OnInteractionReturn temp = new OnInteractionReturn(false,"Je to mimic. To bude bolet!" + GamePlan.GetPlayer().OnPlayerAttacked());
         return temp;
     }
 
+    /**
+     * Metoda vrátí text při útoku @Override
+     *
+     *@return Vratí text při útoku
+     */
     @Override
     public String OnAttack(){
         GamePlan.mimics.remove(this);
