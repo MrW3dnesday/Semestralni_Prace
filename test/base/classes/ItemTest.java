bcc689f9ea82b0561e82273e4c98fbaa868cea5f
MrW3dnesday <31487011+MrW3dnesday@users.noreply.github.com>
2021-05-22 17:33:46 +0100

    Finnished Test Class for BasicRoom

---
+++ test/base/classes/ItemTest.java
@@ -0,0 +1,48 @@
+package base.classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+    void onInspect() {
+    }
+
+    @Test
+    void onUse() {
+    }
+
+    @Test
+    void onInteract() {
+    }
+
+    @Test
+    void getItemName() {
+    }
+
+    @Test
+    void getItemEngineName() {
+    }
+
+    @Test
+    void getItemDescription() {
+    }
+
+    @Test
+    void onItemPutInBackpack() {
+    }
+
+    @Test
+    void onItemTakeOutBackpack() {
+    }
+
+    @Test
+    void onAttack() {
+    }
+
+    @Test
+    void canBeInBackpack() {
+    }
+}
